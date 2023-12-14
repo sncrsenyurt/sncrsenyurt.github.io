8bcfa7034af2ebc92b47dea2f1c0455962831700
sncrsenyurt-Git <sencersenyurt@gmail.com>
2023-12-14 03:04:48 -0800

    new website

---
+++ src/components/About/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.gif")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Data Analyst</h3>
+              <p>
+                I'm a Data Analyst with experience in large product data set and data querying languages (e.g. SQL)
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3></h3>
+              <p>
+                I have experience Experience with tools to analyze, manipulate and visualize datasets such as Tableau
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
